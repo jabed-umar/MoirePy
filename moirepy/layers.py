2a789f440064844b27d731c06dbfe541c339a424
Jabed Umar <111355728+jabed-umar@users.noreply.github.com>
2025-02-04 22:50:34 +0530

    Update layers.py

--- moirepy/layers.py
+++ moirepy/layers.py
@@ -81,13 +81,14 @@ class Layer:  # parent class
             atom_type: [rot_m @ np.array(neighbour) for neighbour in neighbour_list]
             for atom_type, neighbour_list in self.neighbours.items()
         }
+
     def generate_points(
             self,
             mlv1: np.array,
             mlv2: np.array,
             mln1: int = 1,
             mln2: int = 1,  # bring_to_center = False
-            ) -> None:
+    ) -> None:
         """
         Generates points for a Moiré lattice based on the given lattice
         vectors and the number of unit cells along each direction.
@@ -135,8 +136,8 @@ class Layer:  # parent class
         # Step 2: Generate points inside one moire unit cell (based on `lv1` and `lv2`)
         step1_points = []  # List to hold points inside the unit cell
         step1_names = []  # List to hold the names of the points
-        for i in range(-n, n+1):  # Iterate along mlv1
-            for j in range(-n, n+1):  # Iterate along mlv2
+        for i in range(-n, n + 1):  # Iterate along mlv1
+            for j in range(-n, n + 1):  # Iterate along mlv2
                 # Calculate the lattice point inside the unit cell
                 point_o = i * self.lv1 + j * self.lv2
                 for xpos, ypos, name in self.lattice_points:
@@ -321,11 +322,10 @@ class Layer:  # parent class
 
         mask = self._inside_polygon(all_points, np.array([
             (-neigh_pad_1) * v1 + (-neigh_pad_2) * v2,
-            (1+neigh_pad_1) * v1 + (-neigh_pad_2) * v2,
-            (1+neigh_pad_1) * v1 + (1+neigh_pad_2) * v2,
-            (-neigh_pad_1) * v1 + (1+neigh_pad_2) * v2,
-            ])
-        )
+            (1 + neigh_pad_1) * v1 + (-neigh_pad_2) * v2,
+            (1 + neigh_pad_1) * v1 + (1 + neigh_pad_2) * v2,
+            (-neigh_pad_1) * v1 + (1 + neigh_pad_2) * v2,
+        ]))
         print(mask.shape, mask.dtype)
         points = all_points[mask]
         point_names = all_point_names[mask]
@@ -414,10 +414,10 @@ class Layer:  # parent class
                 # plt.plot(*point, "r.")
                 # plt.plot(*self.points[index], "g.")
                 # parallellogram around the whole lattice
-                plt.plot([0, self.mln1*self.mlv1[0]], [0, self.mln1*self.mlv1[1]], 'k', linewidth=1)
-                plt.plot([0, self.mln2*self.mlv2[0]], [0, self.mln2*self.mlv2[1]], 'k', linewidth=1)
-                plt.plot([self.mln1*self.mlv1[0], self.mln1*self.mlv1[0] + self.mln2*self.mlv2[0]], [self.mln1*self.mlv1[1], self.mln1*self.mlv1[1] + self.mln2*self.mlv2[1]], 'k', linewidth=1)
-                plt.plot([self.mln2*self.mlv2[0], self.mln1*self.mlv1[0] + self.mln2*self.mlv2[0]], [self.mln2*self.mlv2[1], self.mln1*self.mlv1[1] + self.mln2*self.mlv2[1]], 'k', linewidth=1)
+                plt.plot([0, self.mln1 * self.mlv1[0]], [0, self.mln1 * self.mlv1[1]], 'k', linewidth=1)
+                plt.plot([0, self.mln2 * self.mlv2[0]], [0, self.mln2 * self.mlv2[1]], 'k', linewidth=1)
+                plt.plot([self.mln1 * self.mlv1[0], self.mln1 * self.mlv1[0] + self.mln2 * self.mlv2[0]], [self.mln1 * self.mlv1[1], self.mln1 * self.mlv1[1] + self.mln2 * self.mlv2[1]], 'k', linewidth=1)
+                plt.plot([self.mln2 * self.mlv2[0], self.mln1 * self.mlv1[0] + self.mln2 * self.mlv2[0]], [self.mln2 * self.mlv2[1], self.mln1 * self.mlv1[1] + self.mln2 * self.mlv2[1]], 'k', linewidth=1)
 
                 # just plot mlv1 and mlv2 parallellogram
                 plt.plot([0, self.mlv1[0]], [0, self.mlv1[1]], 'k', linewidth=1)
@@ -426,7 +426,7 @@ class Layer:  # parent class
                 plt.plot([self.mlv2[0], self.mlv1[0] + self.mlv2[0]], [self.            mlv2[1], self.mlv1[1] + self.mlv2[1]], 'k', linewidth=1)
                 # for index, point in enumerate(self.bigger_points):
                 #     plt.text(*point, f"{index}", fontsize=6)
-                plt.gca().add_patch(plt.Circle(point, distance/2, color='r', fill=False))
+                plt.gca().add_patch(plt.Circle(point, distance / 2, color='r', fill=False))
 
                 plt.grid()
                 plt.show()
@@ -475,8 +475,8 @@ class Layer:  # parent class
             AssertionError: If `self.kdtree` is not initialized.
             AssertionError: If the number of points does not match the number of types.
             ValueError: If a point type is not defined in `self.neighbours`.
-            ValueError: If PBC is enabled and a distance exceeds 
-                        the specified tolerance.
+            ValueError: If PBC is enabled and a distance exceeds
+            the specified tolerance.
 
         Example:
         ```python
@@ -518,7 +518,6 @@ class Layer:  # parent class
 
         return distances_list, indices_list
 
-
     def query(self, points: np.ndarray, k: int = 1) -> Tuple[np.ndarray, np.ndarray]:
         """
         Queries the KDTree for the nearest neighbors of given points and applies PBC if enabled.
@@ -528,9 +527,9 @@ class Layer:  # parent class
             k (int, optional): The number of nearest neighbors to query. Defaults to 1.
 
         Returns:
-            Tuple[np.ndarray, np.ndarray]: 
+            Tuple[np.ndarray, np.ndarray]:
                 - distances (np.ndarray): An (N, k) array containing the distances to the k nearest neighbors.
-                - indices (np.ndarray): An (N, k) array containing the indices of the k nearest neighbors 
+                - indices (np.ndarray): An (N, k) array containing the indices of the k nearest neighbors
                 in `self.points`. If PBC is enabled, the indices are remapped using `self.mappings`.
 
         Raises:
@@ -540,7 +539,7 @@ class Layer:  # parent class
 
         Behavior:
             - If `self.pbc` is False, the function returns the nearest neighbors as given by KDTree.
-            - If `self.pbc` is True, the function applies `self.mappings` to remap indices according 
+            - If `self.pbc` is True, the function applies `self.mappings` to remap indices according
             to periodic boundary conditions.
 
         Example:
@@ -585,7 +584,6 @@ class Layer:  # parent class
         if not self.pbc:
             return distances_list, indices_list
 
-
         # Convert lists back to numpy arrays for PBC
         try:
             distances = np.array(distances_list)
@@ -611,9 +609,9 @@ class Layer:  # parent class
             Defaults to 1.
 
         Returns:
-            Tuple[np.ndarray, np.ndarray]: 
+            Tuple[np.ndarray, np.ndarray]:
                 - distances (np.ndarray): An (N, k) array containing the distances to the k nearest neighbors.
-                - indices (np.ndarray): An (N, k) array containing the indices of the k nearest neighbors 
+                - indices (np.ndarray): An (N, k) array containing the indices of the k nearest neighbors
                 in `self.points`. If PBC is enabled, the indices are remapped using `self.mappings`.
 
         Behavior:
@@ -630,7 +628,7 @@ class Layer:  # parent class
         >>> distances, indices = layer.query_non_self(points, k=2)
         ```
         """
-        distances, indices = self.query(points, k=k+1)
+        distances, indices = self.query(points, k=k + 1)
 
         if self.pbc is False:
             for i in range(len(indices)):
@@ -642,7 +640,6 @@ class Layer:  # parent class
 
         # return distances[:, 1:], indices[:, 1:]
         return distances, indices
-
 
     def plot_lattice(self, plot_connections: bool = True, plot_unit_cell: bool = False) -> None:
         """
@@ -713,12 +710,9 @@ class Layer:  # parent class
         plt.axis("equal")
 
 
-
 # ===============================================
-# ========      some example layers      ======== 
+# ============= some example layers =============
 # ===============================================
-
-
 
 class SquareLayer(Layer):
     def __init__(self, pbc=False) -> None:
@@ -729,36 +723,38 @@ class SquareLayer(Layer):
         )
         self.neighbours = {
             "A": [
-                [1, 0],  # Right
-                [0, 1],  # Up
-                [-1, 0], # Left
-                [0, -1], # Down
+                [1, 0],   # Right
+                [0, 1],   # Up
+                [-1, 0],  # Left
+                [0, -1],  # Down
             ],
         }
         self.study_proximity = 1
         # study_proximity = 1 means only studying nearest neighbours will be eabled, 2 means study of next nearest neighbours will be enabled too and so on
         super().__init__(pbc=pbc)  # this has to go at the end
 
+
 class TriangularLayer(Layer):
     def __init__(self, pbc=False) -> None:
         self.lv1 = np.array([1, 0])  # Lattice vector in the x-direction
-        self.lv2 = np.array([0.5, np.sqrt(3)/2])  # Lattice vector at 60 degrees
+        self.lv2 = np.array([0.5, np.sqrt(3) / 2])  # Lattice vector at 60 degrees
         self.lattice_points = (
             [0, 0, "A"],
         )
         self.neighbours = {
             "A": [
                 [1, 0],  # Right
-                [0.5, np.sqrt(3)/2],  # Right-up
-                [-0.5, np.sqrt(3)/2],  # Left-up
+                [0.5, np.sqrt(3) / 2],  # Right-up
+                [-0.5, np.sqrt(3) / 2],  # Left-up
                 [-1, 0],  # Left
-                [-0.5, -np.sqrt(3)/2],  # Left-down
-                [0.5, -np.sqrt(3)/2],  # Right-down
+                [-0.5, -np.sqrt(3) / 2],  # Left-down
+                [0.5, -np.sqrt(3) / 2],  # Right-down
             ],
         }
         self.study_proximity = 1
         # study_proximity = 1 means only studying nearest neighbours will be eabled, 2 means study of next nearest neighbours will be enabled too and so on
         super().__init__(pbc=pbc)  # this has to go at the end
+
 
 class Rhombus60Layer(Layer):
     def __init__(self, pbc=False) -> None:
